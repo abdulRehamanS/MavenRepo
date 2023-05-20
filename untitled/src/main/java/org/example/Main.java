83903755bde40ae252316743b150c7aa3c9859a8
abdulRehamanS <abdulrehaman627@gmail.com>
2023-05-20 15:51:40 +0530

    solved conflict problem

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -2,11 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
-<<<<<<< HEAD
-        System.out.println("modified by abdul");
-=======
-         System.out.println("Modified in sout");
->>>>>>> f8e3b85704b5c099bf8a2b987b5ba8e821f21403
+	    System.out.println("modified by abdul");
+        System.out.println("Modified in sout");
+
     }
 }
